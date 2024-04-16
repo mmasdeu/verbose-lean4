dfa2a04ef55847fdf7e5b3a0fdd79c69c1c775c9
Marc Masdeu <marc.masdeu@gmail.com>
2024-04-16 13:53:57 +0200

    Added braces

--- lakefile.lean
+++ lakefile.lean
@@ -1,12 +1,13 @@
 import Lake
 open Lake DSL
 
-package verbose where
+package verbose {
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
     ⟨`autoImplicit, false⟩,
     ⟨`relaxedAutoImplicit, false⟩,
     ⟨`pp.proofs.withType, false⟩]
+}
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
