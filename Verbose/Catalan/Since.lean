7eeb6fc4156f4bd3aadc2d2134f993957f3357cd
Marc Masdeu <marc.masdeu@gmail.com>
2024-04-17 23:03:39 +0200

    Changed i to and...

--- Verbose/Catalan/Since.lean
+++ Verbose/Catalan/Since.lean
@@ -41,33 +41,33 @@ example (n : Nat) (h : ∃ k, n = 2*k) : True := by
   trivial
 
 example (n N : Nat) (hn : n ≥ N) (h : ∀ n ≥ N, ∃ k, n = 2*k) : True := by
-  Com que ∀ n ≥ N, ∃ k, n = 2*k , i n ≥ N obtenim k tal que H : n = 2*k
+  Com que ∀ n ≥ N, ∃ k, n = 2*k and n ≥ N obtenim k tal que H : n = 2*k
   trivial
 
 example (P Q : Prop) (h : P ∧ Q)  : Q := by
-  Com que P ∧ Q obtenim (hP : P) , i (hQ : Q)
+  Com que P ∧ Q obtenim (hP : P) and (hQ : Q)
   exact hQ
 
 example (n : ℕ) (hn : n > 2) (P : ℕ → Prop) (h : ∀ n ≥ 3, P n) : True := by
-  Com que ∀ n ≥ 3, P n , i n ≥ 3 obtenim H : P n
+  Com que ∀ n ≥ 3, P n and n ≥ 3 obtenim H : P n
   trivial
 
 example (n : ℕ) (hn : n > 2) (P Q : ℕ → Prop) (h : ∀ n ≥ 3, P n ∧ Q n) : True := by
-  Com que ∀ n ≥ 3, P n ∧ Q n , i n ≥ 3 obtenim H : P n , i H' : Q n
+  Com que ∀ n ≥ 3, P n ∧ Q n and n ≥ 3 obtenim H : P n and H' : Q n
   trivial
 
 example (n : ℕ) (hn : n > 2) (P : ℕ → Prop) (h : ∀ n ≥ 3, P n) : P n := by
-  Com que ∀ n ≥ 3, P n , i n ≥ 3 concloem que P n
+  Com que ∀ n ≥ 3, P n and n ≥ 3 concloem que P n
 
 example (n : ℕ) (hn : n > 2) (P Q : ℕ → Prop) (h : ∀ n ≥ 3, P n ∧ Q n) : P n := by
-  Com que ∀ n ≥ 3, P n ∧ Q n , i n ≥ 3 concloem que P n
+  Com que ∀ n ≥ 3, P n ∧ Q n and n ≥ 3 concloem que P n
 
 example (n : ℕ) (hn : n > 2) (P Q : ℕ → Prop) (h : ∀ n ≥ 3, P n ∧ Q n) : True := by
-  Com que ∀ n ≥ 3, P n ∧ Q n , i n ≥ 3 obtenim H : P n
+  Com que ∀ n ≥ 3, P n ∧ Q n and n ≥ 3 obtenim H : P n
   trivial
 
 example (n : ℕ) (hn : n > 2) (P Q : ℕ → Prop) (h : ∀ n ≥ 3, P n) (h' : ∀ n ≥ 3, Q n) : True := by
-  Com que ∀ n ≥ 3, P n, ∀ n ≥ 3, Q n , i n ≥ 3 obtenim H : P n , i H' : Q n
+  Com que ∀ n ≥ 3, P n, ∀ n ≥ 3, Q n and n ≥ 3 obtenim H : P n and H' : Q n
   trivial
 
 example (P Q : Prop) (h : P → Q) (h' : P) : Q := by
@@ -75,7 +75,7 @@ example (P Q : Prop) (h : P → Q) (h' : P) : Q := by
   exact h'
 
 example (P Q R : Prop) (h : P → R → Q) (hP : P) (hR : R) : Q := by
-  Com que P → R → Q només cal demostrar que P , i R
+  Com que P → R → Q només cal demostrar que P and R
   constructor
   exact hP
   exact hR
